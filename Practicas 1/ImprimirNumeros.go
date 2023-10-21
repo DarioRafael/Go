6f95ebd6f20a9d4602fa0ba0fa5659b150592902
Dario Garcia <dariorafa29@hotmail.com>
2023-10-21 17:27:10 -0600

    Creando un recursivo imprimir

--- Practicas 1/ImprimirNumeros.go
+++ Practicas 1/ImprimirNumeros.go
@@ -2,11 +2,23 @@ package main
 
 import "fmt"
 
+func numero(a int) {
+
+	if a <= 100 {
+		// Imprime el número actual
+		fmt.Println(a)
+		// Llamada recursiva para el siguiente número
+		numero(a + 1)
+	}
+}
+
 func main() {
 
-	for i := 0; i <= 100; i++ {
+	/*for i := 0; i <= 100; i++ {
 
 		fmt.Println(i)
-	}
+	}*/
+
+	numero(1)
 
 }
