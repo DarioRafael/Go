9d9c56983b134c576743c8bf3609bb2b26a051c1
Dario Rafael <dariorafa29@hotmail.com>
2023-10-24 09:35:09 -0600

    Empezando con las operaciones

--- Practicas 1/CalculadoraBasica.go
+++ Practicas 1/CalculadoraBasica.go
@@ -3,12 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	var num, num2 int
+	var num, num2, opcion int
 
-	fmt.Println("Primer digito...")
+	fmt.Print("Primer digito...")
 	fmt.Scanln(&num)
 
-	fmt.Println("Segundo digito...")
+	fmt.Print("Segundo digito...")
 	fmt.Scanln(&num2)
+
+	fmt.Println("1- Suma")
+	fmt.Println("2- Resta")
+	fmt.Println("3- Multiplicación")
+	fmt.Println("4- División")
+	fmt.Println("5- Exponente")
+
+	fmt.Print("Selecciona la opción que gustes...")
+	fmt.Scanln(&opcion)
+
+	if opcion == 1 {
+		fmt.Println(num + num2)
+	} else if opcion == 3 {
+		fmt.Println(num - num2)
+	} else if opcion == 4 {
+		fmt.Println(num * num2)
+	} else if opcion == 5 {
+		fmt.Println(num / num2)
+	} else if opcion == 6 {
+		fmt.Println(num)
+	}
 
 }
