53a1d55423d57ba21a35aabbcea637644e563efc
Dario Rafael <dariorafa29@hotmail.com>
2023-10-24 09:27:14 -0600

    Agregando calculadora básica

---
+++ Practicas 1/CalculadoraBasica.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num, num2 int
+
+	fmt.Println("Primer digito...")
+	fmt.Scanln(&num)
+
+	fmt.Println("Segundo digito...")
+	fmt.Scanln(&num2)
+
+}
