792dcb160d67378e6b7a7fcf87480131a9bb9623
DarioRafael <dariorafa29@hotmail.com>
2023-11-04 19:53:11 -0600

    Probando...

--- Practicas 1/CalcularSalario.go
+++ Practicas 1/CalcularSalario.go
@@ -10,6 +10,9 @@ type Persona struct {
 func main() {
 
 	persona1 := Persona{Salario: 2, Prestaciones: 20}
+	persona2 := Persona{Salario: 2, Prestaciones: 20}
+
 	fmt.Println(persona1)
+	fmt.Println(persona2)
 
 }
